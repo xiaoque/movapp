8699e111984abd22a3bf5a5ea96592ec2b2e865d
xiaoque <xiaoque55@gmail.com>
2014-03-13 12:29:52 +0100

    Revert 4347484..a66ec96
    This rolls back to commit 4347484a127798abb2519632f62895a8babb6014.

--- Login/src/com/example/login/LoginActivity.java
+++ Login/src/com/example/login/LoginActivity.java
@@ -1,21 +1,17 @@
 package com.example.login;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity {
-
-	private EditText mail, pass;
+public class LoginActivity extends Activity{
+	
+	private EditText mail,pass;
 	private Button btn_signup, btn_login;
 
 	@Override
@@ -25,10 +21,10 @@ public class LoginActivity extends Activity {
 		findViews();
 		setListener();
 	}
-
+	
 	private void setListener() {
-		btn_signup.setOnClickListener(new ButtonListener());
-		btn_login.setOnClickListener(new ButtonListener());
+		btn_signup.setOnClickListener(signupListener);
+		btn_login.setOnClickListener(loginListener);
 	}
 
 	private void findViews() {
@@ -37,68 +33,32 @@ public class LoginActivity extends Activity {
 		mail = (EditText) findViewById(R.id.login_email);
 		pass = (EditText) findViewById(R.id.login_pass);
 	}
-
-	private class ButtonListener implements OnClickListener {
+	
+	private OnClickListener signupListener = new OnClickListener() {
+		@Override
 		public void onClick(View v) {
-			switch (v.getId()) {
-			case R.id.btn_login:
-				valideInput();
-				break;
-			case R.id.btn_signup: {
-				Intent intent = new Intent();
-				intent.setClass(LoginActivity.this, SignupActivity.class);
-				startActivity(intent);
-				break;
-			}
-
-			}
+			Intent intent = new Intent();
+			intent.setClass(LoginActivity.this, SignupActivity.class);
+			startActivity(intent);
 		}
-
-	}
-
-	public void valideInput() {
-		if ("".equals(mail.getText().toString())
-				|| "".equals(pass.getText().toString())) {
-			Toast toast = Toast.makeText(LoginActivity.this,
-					"Please fill all fields", 1000);
-			toast.setGravity(Gravity.CENTER, 0, 0);
-			toast.show();
-
-		} else {
+	};
+	
+	private OnClickListener loginListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
 			String str_mail = mail.getText().toString().trim();
 			String str_pass = pass.getText().toString().trim();
-			if (isEmailValid(str_mail)) {
-				Toast.makeText(LoginActivity.this, str_mail, 1000).show();
-				Toast.makeText(LoginActivity.this, str_pass, 1000).show();
-				Intent intent = new Intent();
-				intent.setClass(LoginActivity.this, ScanActivity.class);
-				startActivity(intent);
-			} else {
-				Toast toast = Toast.makeText(LoginActivity.this,
-						"Please enter a valid email address", 1000);
-				toast.setGravity(Gravity.CENTER, 0, 0);
-				toast.show();
-			}
+			Toast.makeText(LoginActivity.this, str_mail, 1000).show();
+			Toast.makeText(LoginActivity.this, str_pass, 1000).show();
+			Intent intent = new Intent();
+			intent.setClass(LoginActivity.this, ScanActivity.class);
+			startActivity(intent);
 		}
-	}
-
-	public boolean isEmailValid(String email) {
-		String regExpn = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-				+ "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-				+ "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-				+ "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-				+ "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-				+ "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
-
-		CharSequence inputStr = email;
-
-		Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher(inputStr);
-
-		if (matcher.matches())
-			return true;
-		else
-			return false;
+	};
+	
+	public int testInput(String strmail, String strpass){
+		return 0;
 	}
 
 }
+
--- Login/src/com/example/login/SignupActivity.java
+++ Login/src/com/example/login/SignupActivity.java
@@ -1,89 +1,14 @@
 package com.example.login;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class SignupActivity extends Activity{
-	private EditText email, pass, passconfirm;
-	private Button signup;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.setContentView(R.layout.activity_signup);
-		findViews();
-		setListener();
 	}
 	
-	private void findViews() {
-		email = (EditText) findViewById(R.id.signup_email);
-		pass = (EditText) findViewById(R.id.signup_pass);
-		passconfirm = (EditText) findViewById(R.id.signup_passconfirm);
-	}
-	private void setListener() {
-		signup.setOnClickListener(signupListener);
-
-	}
-	private OnClickListener signupListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			if("".equals(email.getText().toString())|| "".equals(pass.getText().toString())||"".equals(passconfirm.getText().toString())){
-				Toast toast = Toast.makeText(SignupActivity.this,
-						"Please fill all fields", 1000);
-				toast.setGravity(Gravity.CENTER, 0, 0);
-				toast.show();
-			}
-			else{
-				if((pass.getText().toString().trim()).equals(passconfirm.getText().toString().trim())){
-					String str_mail = email.getText().toString().trim();
-					if(isEmailValid(str_mail)){
-						String str_pass = pass.getText().toString().trim();
-						Intent intent = new Intent();
-						intent.setClass(SignupActivity.this, ScanActivity.class);
-						startActivity(intent);
-					}
-					else{
-						Toast toast = Toast.makeText(SignupActivity.this,
-								"Please enter a valid email address", 1000);
-						toast.setGravity(Gravity.CENTER, 0, 0);
-						toast.show();
-					}
-				}else{
-					Toast toast = Toast.makeText(SignupActivity.this,
-							"Password confirm should be equal to password", 1000);
-					toast.setGravity(Gravity.CENTER, 0, 0);
-					toast.show();
-				}
-			}
-		}	
-	};
-
-	public boolean isEmailValid(String email) {
-		String regExpn = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-				+ "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-				+ "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-				+ "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-				+ "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-				+ "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
-
-		CharSequence inputStr = email;
-
-		Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher(inputStr);
-
-		if (matcher.matches())
-			return true;
-		else
-			return false;
-	}
-
 }
