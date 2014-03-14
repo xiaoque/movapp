7617ff8987634b58aa68de6f8a9aa81831c0647d
xiaoque <xiaoque55@gmail.com>
2014-03-14 23:15:41 +0100

    add signup input verify

--- Login/gen/com/example/login/R.java
+++ Login/gen/com/example/login/R.java
@@ -47,15 +47,18 @@ public final class R {
         public static final int welcome_android=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int allrightsreserved=0x7f090007;
+        public static final int action_settings=0x7f09000b;
+        public static final int allrightsreserved=0x7f09000a;
         public static final int btn_login=0x7f090004;
         public static final int btn_signup=0x7f090005;
         public static final int linear1=0x7f090001;
-        public static final int login=0x7f090006;
         public static final int login_email=0x7f090002;
         public static final int login_logo=0x7f090000;
         public static final int login_pass=0x7f090003;
+        public static final int signup_mail=0x7f090006;
+        public static final int signup_ok=0x7f090009;
+        public static final int signup_pass=0x7f090007;
+        public static final int signup_passconfirm=0x7f090008;
     }
     public static final class layout {
         public static final int activity_app_start=0x7f030000;
