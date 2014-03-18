7eb252a4a04c5f758f78aa47ec9b60bc730b7a70
xiaoque <xiaoque55@gmail.com>
2014-03-18 14:23:45 +0100

    qrcode get result

--- scanner/src/com/androidaz/scanner/ScannerActivity.java
+++ scanner/src/com/androidaz/scanner/ScannerActivity.java
@@ -1,17 +1,16 @@
 package com.androidaz.scanner;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
 import com.google.zxing.client.android.CaptureActivity;
 
-public class ScannerActivity extends CaptureActivity {
+public class ScannerActivity extends Activity {
 	private Button button1;
 
 	/** Called when the activity is first created. */
@@ -23,27 +22,26 @@ public class ScannerActivity extends CaptureActivity {
 		button1.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(
-						"com.google.zxing.client.android.SCAN");
+				Intent intent = new Intent(ScannerActivity.this,
+						CaptureActivity.class);
 				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-				startActivityForResult(intent, 0);
+				startActivityForResult(intent, 1);
 			}
 		});
 	}
 
+	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == 0) {
-			if (resultCode == RESULT_OK) {
-				String capturedQrValue = intent.getStringExtra("RESULT");
-				// String format =
-				intent.getStringExtra("SCAN_RESULT_FORMAT");
-				Toast.makeText(ScannerActivity.this,
-						"Scan Result:" + capturedQrValue, Toast.LENGTH_SHORT)
-						.show();
-				// Handle successful scan
-			} else if (resultCode == RESULT_CANCELED) {
-				// Handle cancel
-			}
+		if (resultCode == RESULT_OK) {
+			String capturedQrValue = intent.getStringExtra("scan_result");
+			// String format =
+			intent.getStringExtra("SCAN_RESULT_FORMAT");
+			Toast.makeText(ScannerActivity.this,
+					"Scan Result:" + capturedQrValue, Toast.LENGTH_SHORT)
+					.show();
+			// Handle successful scan
+		} else if (resultCode == RESULT_CANCELED) {
+			// Handle cancel
 		}
 	}
 
